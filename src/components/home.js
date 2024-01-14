3b7ce0dea6021f3c77bfd60ef76ff84c1faa1f39
TemoshoS <temoshomaduane@gmail.com>
2024-01-14 23:24:42 +0200

    contact

--- src/components/home.js
+++ src/components/home.js
@@ -1,4 +1,5 @@
-import React, { useEffect } from 'react';
+import React, {useState, useEffect,useRef } from 'react';
+import emailjs from '@emailjs/browser';
 import NavBar from '../components/navBar';
 import 'boxicons';
 import Typed from 'typed.js';
@@ -6,8 +7,37 @@ import profile from '../profile.jpg';
 import voice from '../images/voice.jpg';
 import country from '../images/country.jpg';
 import weather from '../images/weather.jpg';
+import '@fortawesome/fontawesome-free/css/all.css';
+
+
 
 function Home() {
+ const [loading, setLoading] = useState(false);
+ const form = useRef();
+
+ const sendEmail = (e) => {
+  setLoading(true); 
+  e.preventDefault();
+
+  emailjs
+    .sendForm('service_6v41z8s', 'template_xtapfn8', form.current, 'gfSpZlYXHAxPUBS1v')
+    .then(
+      (result) => {
+        setLoading(false); 
+        console.log(result.text)
+        form.current.reset();
+        
+        alert('Message sent successfully!');
+      },
+      (error) => {
+        console.log(error.text);
+        alert('Error sending message. Please try again later.');
+      }
+    );
+    
+};
+
+
   useEffect(() => {
     const options = {
       strings: ['Mobile developer', 'Front-end developer', 'back-end', 'web developer'],
@@ -24,34 +54,48 @@ function Home() {
     };
   }, []);
 
+
+  const handleDownloadCV = () => {
+
+    const pdfUrl = "Temosho.pdf";
+    const link = document.createElement("a");
+    link.href = pdfUrl;
+    link.download = "Temosho_Shaku_resume.pdf";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
   return (
     <div >
       <NavBar />
       {/* Landing Section */}
-      <div id='home' className='section'>
-        <div className='home'>
-          <div className='home-content'>
-            <div className='content-text'>
-              <h3>Hello, It's Me</h3>
-              <h1>Temosho Shaku</h1>
-              <h3>And I'm a <span className='text'></span></h3>
-              <p> I'm a Software developer with extensive experience for over 2 years.
+      <div id='home' className='home'>
+        <div className='home-content'>
+          <div className='content-text'>
+            <h3>Hello, my name is</h3>
+            <h1>Temosho Shaku</h1>
+            <h3>I'm a <span className='text'></span></h3>
+            {/* <p> I'm a Software developer with extensive experience for over 2 years.
                 expertise is to create and website design, frontend design and...
-              </p>
-              <div className='home-sci'>
-                <a href='https://github.com/TemoshoS' style={{ '--1': 1 }}><i className='bx bxl-github'></i></a>
-                <a href='https://www.linkedin.com/in/temosho-shaku-a2598917b/' style={{ '--1': 2 }}><i className='bx bxl-linkedin-square'></i></a>
-                <a href='https://www.facebook.com/temosho.shaku' style={{ '--1': 3 }}><i className='bx bxl-facebook-square'></i></a>
-                <a href='#' style={{ '--1': 4 }}><i className='bx bxl-twitter'></i></a>
-              </div>
-              <a href='#about' className='btnMore'>More About </a>
+              </p> */}
+            <div className='home-sci'>
+              <a href='https://github.com/TemoshoS' style={{ '--1': 1 }}><i className='bx bxl-github'></i></a>
+              <a href='https://www.linkedin.com/in/temosho-shaku-a2598917b/' style={{ '--1': 2 }}><i className='bx bxl-linkedin-square'></i></a>
+              <a href='https://www.facebook.com/temosho.shaku' style={{ '--1': 3 }}><i className='bx bxl-facebook-square'></i></a>
+
+            </div>
+            <div>
+              <button className='btnMore' onClick={handleDownloadCV}>Download CV</button>
+              <a href='#about' className='btnMore'>More About Me</a>
             </div>
           </div>
-          <div className='profile-image'>
-            <img src={profile} alt='Profile' />
-          </div>
+        </div>
+        <div className='profile-image'>
+          <img src={profile} alt='Profile' />
         </div>
       </div>
+
 
       {/* About Section */}
       <div id='about' className='about'>
@@ -64,7 +108,7 @@ function Home() {
           <h4>Full Stack Developer</h4>
           <p>I'm Temosho Shaku, a seasoned Web and Mobile React Developer proudly based in the dynamic cities of Tshwane and Johannesburg, Gauteng, South Africa.
             Currently immersed as a React Software Developer Trainee at mLab Southern Africa, I specialize in crafting seamless digital experiences using technologies like React.js, React Native, Angular, Java, SQL, and Node.js.
-            My journey includes diverse experiences, from internships at MOEPI PUBLISHING and Dynamic DNA to contributing to innovative projects like an ecommerce website in React.js and Firebase.
+            My journey includes diverse experiences, from internships at MOEPI PUBLISHING and Dynamic DNA to contributing to innovative services like an ecommerce website in React.js and Firebase.
             With a National Diploma in Information Technology from Tshwane University of Technology, I'm driven by a passion for turning creative concepts into high-quality software solutions.
             Explore my portfolio to witness my dedication to user-friendly and problem-solving development.</p>
         </div>
@@ -72,58 +116,38 @@ function Home() {
 
       </div>
 
-      {/* Projects Section */}
-      <div id='projects' className='projects' style={{backgroundColor:'black'}}>
+      {/* services Section */}
+      <div id='services' className='services' style={{ backgroundColor: 'black' }}>
         <div className='container'>
-          <h1 className='sub-title'>My <span>Projects</span></h1>
-          <div className='projects-list'>
-            <div>
-              <img src={voice} className='project-image' alt='project image'/>
-              <h2>Voice Journal App</h2>
+          <h1 className='sub-title'>My <span>services</span></h1>
+          <div className='services-list'>
+            <div className="center-content">
+              <i className='services-icons bx bx-code-alt'></i>
+              <h2>Web Development</h2>
               <p>A digital journal app for voice recording. The app allow users to create, listen to, and
                 delete voice notes.</p>
-                <a href='#' className='read'>Learn more</a>
+              <a href='#' className='read'>Learn more</a>
             </div>
 
-            <div>
-            <img src={country} className='project-image' alt='project image'/>
-              <h2>Voice Journal App</h2>
+            <div class="center-content">
+              <i className='services-icons bx bx-mobile'></i>
+              <h2>Mobile Development</h2>
               <p>A digital journal app for voice recording. The app allow users to create, listen to, and
                 delete voice notes.</p>
-                <a href='#' className='read'>Learn more</a>
+              <a href='#' className='read'>Learn more</a>
             </div>
 
-            <div>
-            <img src={weather} className='project-image' alt='project image'/>
-              <h2>Voice Journal App</h2>
+            <div class="center-content">
+              <i className='services-icons bx bxl-apple'></i>
+              <h2>UI/UX Design</h2>
               <p>A digital journal app for voice recording. The app allow users to create, listen to, and
                 delete voice notes.</p>
-                <a href='#' className='read'>Learn more</a>
+              <a href='#' className='read'>Learn more</a>
             </div>
 
-            <div>
-            <img src={voice} className='project-image' alt='project image'/>
-              <h2>Voice Journal App</h2>
-              <p>A digital journal app for voice recording. The app allow users to create, listen to, and
-                delete voice notes.</p>
-                <a href='#' className='read'>Learn more</a>
-            </div>
 
-            <div>
-            <img src={voice} className='project-image' alt='project image'/>
-              <h2>Voice Journal App</h2>
-              <p>A digital journal app for voice recording. The app allow users to create, listen to, and
-                delete voice notes.</p>
-                <a href='#' className='read'>Learn more</a>
-            </div>
 
-            <div>
-            <img src={voice} className='project-image' alt='project image'/>
-              <h2>Voice Journal App</h2>
-              <p>A digital journal app for voice recording. The app allow users to create, listen to, and
-                delete voice notes.</p>
-                <a href='#' className='read'>Learn more</a>
-            </div>
+
 
           </div>
 
@@ -134,17 +158,259 @@ function Home() {
       {/* Skills Section */}
       <div id='skills' className='skills'>
 
-        <text>skilss</text>
+        <div  className='container1'>
+          <h1 className='heading1'>Technical Skills</h1>
+          <div className='Technical-bars'>
+            <div className='bar'><i style={{ color: 'orange' }} class='bx bxl-html5'></i>
+              <div className='info'>
+                <span>HTML</span>
+
+              </div>
+              <div className='progress-line html'>
+                <span></span>
+
+              </div>
+            </div>
+            <div className='bar'><i style={{ color: 'blue' }} class='bx bxl-css3'></i>
+              <div className='info'>
+                <span>CSS</span>
+
+              </div>
+              <div className='progress-line css'>
+                <span></span>
+
+              </div>
+            </div>
+            <div className='bar'><i style={{ color: 'yellow' }} class='bx bxl-javascript'></i>
+              <div className='info'>
+                <span>JavaScript</span>
+
+              </div>
+              <div className='progress-line javascript'>
+                <span></span>
+
+              </div>
+            </div>
+
+            <div className='bar'><i style={{ color: 'blue' }} class='bx bxl-react'></i>
+              <div className='info'>
+                <span>React</span>
+
+              </div>
+              <div className='progress-line react'>
+                <span></span>
+
+              </div>
+            </div>
+            <div className='bar'><i style={{ color: 'green' }} class='bx bxl-nodejs'></i>
+              <div className='info'>
+                <span>Node js</span>
+
+              </div>
+              <div className='progress-line node'>
+                <span></span>
+
+              </div>
+            </div>
+            <div className='bar'><i style={{ color: '#A020F0' }} class='bx bxl-redux'></i>
+              <div className='info'>
+                <span>Redux</span>
+
+              </div>
+              <div className='progress-line redux'>
+                <span></span>
+
+              </div>
+            </div>
+            <div className='bar'><i style={{ color: 'red' }} class='bx bxl-java'></i>
+              <div className='info'>
+                <span>Java</span>
+
+              </div>
+              <div className='progress-line java'>
+                <span></span>
+
+              </div>
+            </div>
+            <div className='bar'><i style={{ color: '#CCCC00' }} class='bx bxl-firebase'></i>
+              <div className='info'>
+                <span>Firebase</span>
+
+              </div>
+              <div className='progress-line firebase'>
+                <span></span>
+
+              </div>
+            </div>
+            <div className='bar'><i style={{ color: 'green' }} class='bx bxs-data'></i>
+              <div className='info'>
+                <span>MySQL</span>
+
+              </div>
+              <div className='progress-line sql'>
+                <span></span>
+
+              </div>
+            </div>
+            <div className='bar'><i style={{ color: 'white' }}class='bx bxl-github'></i>
+              <div className='info'>
+                <span>Git</span>
+
+              </div>
+              <div className='progress-line git'>
+                <span></span>
+
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+        <div className='container2'>
+          <h1 className='heading1'>Professional Skilss</h1>
+
+          <div className='radial-bars'>
+
+            <div className='radial-bar'>
+
+              <svg x='0px' y='0px' viewBox='0 0 200 200'>
+                <circle className='progress-bar' cx='100' cy='100' r='80'></circle>
+                <circle className='path path-1' cx='100' cy='100' r='80'></circle>
+              </svg>
+              <div className='percentage'>90%</div>
+              <div className='text'>Creativity</div>
+
+            </div>
+            <div className='radial-bar'>
+
+              <svg x='0px' y='0px' viewBox='0 0 200 200'>
+                <circle className='progress-bar' cx='100' cy='100' r='80'></circle>
+                <circle className='path path-2' cx='100' cy='100' r='80'></circle>
+              </svg>
+              <div className='percentage'>65%</div>
+              <div className='text'>Communication</div>
+
+            </div>
+            <div className='radial-bar'>
+
+              <svg x='0px' y='0px' viewBox='0 0 200 200'>
+                <circle className='progress-bar' cx='100' cy='100' r='80'></circle>
+                <circle className='path path-3' cx='100' cy='100' r='80'></circle>
+              </svg>
+              <div className='percentage'>75%</div>
+              <div className='text'>Problem Solving</div>
+
+            </div>
+            <div className='radial-bar'>
+
+              <svg x='0px' y='0px' viewBox='0 0 200 200'>
+                <circle className='progress-bar' cx='100' cy='100' r='80'></circle>
+                <circle className='path path-4' cx='100' cy='100' r='80'></circle>
+              </svg>
+              <div className='percentage'>85%</div>
+              <div className='text'>Teamwork</div>
+
+            </div>
+          </div>
+
+        </div>
 
       </div>
 
+      {/* Projects Section */}
+      <div id='projects' className='projects'>
+        <h1>projects</h1>
 
+      </div>
 
       {/* Contact Section */}
-      <div id='contact' className='section'>
-        {/* Add your contact content here */}
-        <text>skilss</text>
+      <div className='contact' id='contact'>
+        <div className='content'>
+          <h2 >Contact Us</h2>
+          <p>Let's talk, i will get back to you</p>
       </div>
+      <div className='container3'>
+        <div className='contactInfo'>
+          <div className='box'>
+            <div className='icon'><i class="fa-solid fa-location-dot"></i></div>
+            <div className='text'>
+              <h3>Adrress</h3>
+              <p>50 ganchabeleng,<br></br>ganchabeleng<br></br>0741</p>
+
+            </div>
+            </div>
+            <div className='box'>
+            <div className='icon'><i class="fa-solid fa-phone"></i></div>
+            <div className='text'>
+              <h3>Phone</h3>
+              <p>0721371977</p>
+
+            </div>
+            </div>
+            <div className='box'>
+            <div className='icon'><i class="fa-regular fa-envelope"></i></div>
+            <div className='text'>
+              <h3>Email</h3>
+              <p>temoshomaduane@gmail.com</p>
+
+            </div>
+            </div>
+
+
+        </div>
+        <div className='contactForm'>
+        <form
+    
+    ref={form} onSubmit={sendEmail}
+  >
+    <h2>Send Message</h2>
+    <div className="inputBox">
+      <input
+        type="text"
+        name="user_name"
+        required
+      />
+      <span>Full Name</span>
+    </div>
+    <div className="inputBox">
+      <input
+        type="email"
+        name="user_email"
+        required
+      />
+      <span>Email</span>
+    </div>
+    
+    <div className="inputBox">
+      <input
+        type="subject"
+        name="subject"
+        required
+      />
+      <span>Subject</span>
+    </div>
+    <div className="inputBox">
+      <textarea
+        name="message"
+        required
+      />
+      <span>Type your message...</span>
+    </div>
+    <div className="inputBox">
+      <button
+        type="submit"
+      >
+        Send
+      </button>
+    </div>
+  </form>
+        </div>
+
+      </div>
+      </div>
+      
+
+
     </div>
   );
 }
--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,4 +1,3 @@
-// NavBar.js
 
 import React, { useState, useEffect } from 'react';
 import logo from '../logo-white.png';
@@ -37,15 +36,19 @@ function NavBar() {
             </svg>
           </a>
         </li>
+        <li><a href='#home'>Home</a></li>
+        <li><a href='#services'>Services</a></li>
         <li><a href='#skills'>Skills</a></li>
         <li><a href='#projects'>Projects</a></li>
         <li><a href='#contact'>Contact</a></li>
       </ul>
       <ul>
         <li><a href='#home'><img src={logo} alt="Logo" className='logo' />Temosho</a></li>
-        <li className='hideOnMobile'><a href='#skills' style={{ '--1': 1 }}>Skills</a></li>
-        <li className='hideOnMobile'><a href='#projects' style={{ '--1': 2 }}>Projects</a></li>
-        <li className='hideOnMobile'><a href='#contact' style={{ '--1': 3 }}>Contact</a></li>
+        <li className='hideOnMobile'><a href='#home' style={{ '--1': 1 }}>Home</a></li>
+        <li className='hideOnMobile'><a href='#services' style={{ '--1': 2 }}>Services</a></li>
+        <li className='hideOnMobile'><a href='#skills' style={{ '--1': 3 }}>Skills</a></li>
+        <li className='hideOnMobile'><a href='#projects' style={{ '--1': 4 }}>Projects</a></li>
+        <li className='hideOnMobile'><a href='#contact' style={{ '--1': 5 }}>Contact</a></li>
         <li className='menu-button' onClick={showSidebar}>
           <a href="#">
             <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24">
